8712b3e8708d3d9d8d298d4b9409963a4b356b7d
MbheleLonwabo <owenmbhele92@gmail.com>
2026-05-10 04:29:34 +0200

    Update comment for Sellable interface clarity

--- app/src/main/java/com/product/TextbookSeller/Sellable.java
+++ app/src/main/java/com/product/TextbookSeller/Sellable.java
@@ -1,6 +1,6 @@
 package com.product.TextbookSeller;
 /**
- * Interface defining what it means for an item to be sellable in our app.
+ * Interface defining what it means for an item to be sellable in our application.
  */
 public interface Sellable {
     double getPrice();
